42edeef03cc4c583beb98931c9a7f7645835e0de
Gustavo Ueti <gustavo.ueti@gmail.com.br>
2021-11-26 15:36:16 -0300

    refactor: :recycle: updating api route

--- src/main.js
+++ src/main.js
@@ -13,7 +13,8 @@ Vue.use(VueRouter);
 Vue.use(VueMaterial);
 Vue.use(Vuelidate);
 
-Vue.prototype.$apiRoute = 'https://adinfo.ue.r.appspot.com';
+// Vue.prototype.$apiRoute = 'https://api-dot-adinfo-diageo.uc.r.appspot.com/';
+Vue.prototype.$apiRoute = 'https://adinfo-diageo.uc.r.appspot.com/';
 // Vue.prototype.$apiRoute = 'http://localhost:8000';
 
 const router = new VueRouter({
