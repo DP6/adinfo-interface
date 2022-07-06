ed8fe0cf20cf96d18f11d563c1038801468a8fab
Fabiane Souza <fabianesouza9.68@gmail.com>
2022-07-06 17:01:32 -0300

    Adicionar botão na aba "Nova Configuração" para baixar a última configuração adicionada.

--- src/components/nova_configuracao/NovaConfiguracao.vue
+++ src/components/nova_configuracao/NovaConfiguracao.vue
@@ -18,6 +18,23 @@
                 </md-card-actions>
             </md-card>
         </form>
+        <md-card-content></md-card-content>
+        <form class="md-layout">
+            <md-card class="md-layout-item md-larger-size">
+                <md-card-content>
+                    <div class="md-layout md-gutter">
+                        <div class="md-layout-item md-medium-size-100">
+                            <md-field>
+                                <label>Baixe o a última Configuração</label>
+                            </md-field>
+                        </div>
+                    </div>
+                </md-card-content>
+                <md-card-actions>
+                    <botao-submit @botaoAtivado="downloadConfig()" nome_do_botao="Baixar Configuração"></botao-submit>
+                </md-card-actions>
+            </md-card>
+        </form>
         <div class="load" v-show="show_load">
             <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
         </div>
@@ -45,10 +62,10 @@ export default {
     name: 'CSVForm',
     mixins: [validationMixin],
     components: {
-        'titulo-principal': TituloAreaPrincipal,
-        'botao-submit': BotaoSubmitForm,
-        'usuario-invalido': InvalidUserAlert
-    },
+    'titulo-principal': TituloAreaPrincipal,
+    'botao-submit': BotaoSubmitForm,
+    'usuario-invalido': InvalidUserAlert
+},
     data() {
         return {
             showSnackbar: false,
@@ -110,20 +127,41 @@ export default {
                 });
             }
         },
-        downloadParametrizacao() {
-            const url = window.URL.createObjectURL(this.builderFile);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = `${document.querySelector('#file').files[0].name.replace(/\.csv$/, '')}_parametrizado.csv`;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
+        downloadConfig() {
+            const urlConfig = `${this.$apiRoute}/config`;   
+            fetch(urlConfig, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    token: localStorage.getItem('userToken')
+                }
+            }).then((response) => {                
+                this.statusCode = response.status;
+                return response.json();
+            }).then((response) => {             
+                if(this.statusCode === 401) {
+                    this.$router.push('login');
+                } else if(this.statusCode !== 200) {
+                    throw new Error(response.rescreateObjectURLponseText || response.errorMessage);
+                }
+                this.fileConfig = response.responseText;
+                this.fileConfig = JSON.parse(this.fileConfig);
+                return this.fileConfig;
+            }).then((response) => {
+                const url = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(response));
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'config.json';
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            });
         },
         isAuthError(statusCode){
             if(statusCode === 403)
                 return true;
             return false;
-        }
+        }        
     }
 }
 
